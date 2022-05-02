45926227038a3da21d4da61e70d87250cde74d2c
Luis <launchx04206@innovaccion.mx>
2022-05-02 17:42:56 -0500

    9. Agregando index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const Reader = require ("./lib/utils/Reader")
+//const ExplorerService = require("./lib/services/ExplorerService")
+//const FizzBuzzService = require("./lib/services/FizzbuzzService")
+
+const explorers = Reader.readJsonFile("explorers.json")
+//console.log(explorers)
+//console.log( ExplorerService.filterByMission(explorers, "node"))
+//console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node").length)
+//console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
+//const explorer1 = {name: "Explorer1", score: 13}
+//console.log(FizzBuzzService.applyValidationInExplorer(explorer1))
