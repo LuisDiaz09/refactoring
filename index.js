468293d4d1c02cbb13e03636c4dd32f9fccd56ca
Luis <launchx04206@innovaccion.mx>
2022-05-02 22:15:06 -0500

    Corriendo Linter en el proyecto

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const Reader = require ("./lib/utils/Reader")
+const Reader = require ("./lib/utils/Reader");
 //const ExplorerService = require("./lib/services/ExplorerService")
 //const FizzBuzzService = require("./lib/services/FizzbuzzService")
 
-const explorers = Reader.readJsonFile("explorers.json")
+const explorers = Reader.readJsonFile("explorers.json");
 //console.log(explorers)
 //console.log( ExplorerService.filterByMission(explorers, "node"))
 //console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node").length)
