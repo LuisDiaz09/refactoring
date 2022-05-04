90236f69f1b3b9f66d9890d7026cbaf8702790b1
Luis <launchx04206@innovaccion.mx>
2022-05-03 19:03:45 -0500

    33. Modificando index.js con la clase ExplorerController

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Reader = require ("./lib/utils/Reader");
 //const ExplorerService = require("./lib/services/ExplorerService")
 //const FizzBuzzService = require("./lib/services/FizzbuzzService")
+const ExplorerController = require("./lib/controllers/ExplorerController")
 
-const explorers = Reader.readJsonFile("explorers.json");
+//const explorers = Reader.readJsonFile("explorers.json");
 //console.log(explorers)
-//console.log( ExplorerService.filterByMission(explorers, "node"))
-//console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node").length)
+console.log( ExplorerController.getExplorersByMission("java"))
+//console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
 //console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
 //const explorer1 = {name: "Explorer1", score: 13}
 //console.log(FizzBuzzService.applyValidationInExplorer(explorer1))
